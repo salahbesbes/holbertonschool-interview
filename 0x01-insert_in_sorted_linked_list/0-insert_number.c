f4697652deeebb71c30b9cb1b8ebd88c0078569b
salahbesbes <salah_besbes@ymail.com>
2021-07-31 16:26:26 +0200

    insert in a sorted single LinkedList

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -4,56 +4,44 @@
 #include <limits.h>
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new;
-    listint_t *current;
-    listint_t *next_node;
-    listint_t *tmp;
-    current = *head;
+	listint_t *new, *current, *next_node, *tmp;
 
-    if (*head == NULL)
-        return NULL;
-    new = malloc(sizeof(listint_t));
-    new->n = number;
-
-    next_node = current->next;
-
-    /* number is the smallest in the linkedList*/
-    if (current->n >= number)
-    {
-        new->next = current;
-        *head = new;
-        return new;
-    }
-
-    /* Linked List only contain one Node*/
-    if (next_node == NULL)
-    {
-        if (number >= current->n)
-        {
-            current->next = new;
-        }
-        else
-        {
-            new->next = current;
-            *head = new;
-        }
-        return new;
-    }
-
-    /* linkedList contain one or make nodes */
-    while (current->next != NULL)
-    {
-        next_node = current->next;
-        if (number > current->n && number <= next_node->n)
-        {
-            printf("current %d next %d\n", current->n, next_node->n);
-            tmp = current->next;
-            current->next = new;
-            new->next = tmp;
-            return new;
-        }
-        current = next_node;
-    }
-    current->next = new;
-    return new;
+	current = *head;
+	if (*head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+	next_node = current->next;
+	if (current->n >= number)
+	{
+		new->next = current;
+		*head = new;
+		return (new);
+	}
+	if (next_node == NULL)
+	{
+		if (number >= current->n)
+			current->next = new;
+		else
+		{
+			new->next = current;
+			*head = new;
+		}
+		return (new);
+	}
+	while (current->next != NULL)
+	{
+		next_node = current->next;
+		if (number > current->n && number <= next_node->n)
+		{
+			printf("current %d next %d\n", current->n, next_node->n);
+			tmp = current->next;
+			current->next = new;
+			new->next = tmp;
+			return (new);
+		}
+		current = next_node;
+	}
+	current->next = new;
+	return (new);
 }
