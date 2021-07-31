810e5c66e2675fea8dfbabb3ff6ffeaf16517e50
salahbesbes <salah_besbes@ymail.com>
2021-07-31 17:52:16 +0200

    insert node in sorted single list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -2,9 +2,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+
+/**
+ * recursive_way - chech if node is at the position we search for
+ * @head: pointer to list to be freed
+ * @current: current node
+ * @new: node to insert
+ * @number: number to check for position
+ * Return: new node inserted
+ */
+listint_t *recursive_way(listint_t **head, listint_t *current,
+						 listint_t *new, int number)
+{
+	listint_t *tmp;
+
+	if (number <= current->n)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	else if (current->next == NULL)
+	{
+		current->next = new;
+		return (new);
+	}
+	else if (current->n <= number && current->next->n >= number)
+	{
+		tmp = current->next;
+		current->next = new;
+		new->next = tmp;
+		return (new);
+	}
+	return (recursive_way(head, current->next, new, number));
+}
+/**
+ * insert_node - recursive way
+ * @head: pointer to list to be freed
+ * @number: nuber to insert in the node
+ * Return: new node inserted
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *current, *next_node, *tmp;
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+
+	return (recursive_way(head, *head, new, number));
+}
+
+/*
+	*listint_t *new, *current, *next_node, *tmp;
 
 	current = *head;
 	if (*head == NULL)
@@ -44,4 +95,4 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 	current->next = new;
 	return (new);
-}
+*/
