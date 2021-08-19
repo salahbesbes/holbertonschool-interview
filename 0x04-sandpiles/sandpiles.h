c30168b340a52bd25a0f2d0a43132c7c91168812
salahbesbes <salah_besbes@ymail.com>
2021-08-19 14:44:01 +0100

    calculate 2 sandpiles

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -4,12 +4,8 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void print_grid_sum(int grid1[3][3], int grid2[3][3]);
 void print_grid(int grid[3][3]);
-
-typedef struct coord
-{
-    int x;
-    int y;
-
-} coord_t;
-
+void stretch(int gr[3][3]);
+int check_before_stretch(int gr[3][3]);
+int calculate_sum(int grid1[3][3], int grid2[3][3]);
+void init_corresponded_grid(int corres[3][3]);
 #endif
