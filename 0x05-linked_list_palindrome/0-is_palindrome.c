df601f6468f6f4ee92b751e962c1b0e733db6e26
salahbesbes <salah_besbes@ymail.com>
2021-08-27 16:42:58 +0200

    check if the single linked list is palindromique

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 
 /**
- * get_node_number - get the value of the node at some position
- * @head: header node
- * @node_Number: position where to stop
- *
- * Return: value of some node at some position
- */
-int get_node_number(listint_t **head, int node_Number)
-{
-	int counter = 1;
-	listint_t *current;
-
-	current = *head;
-	while (counter != node_Number)
-	{
-		current = current->next;
-		counter++;
-	}
-
-	return (current->n);
-}
-
-/**
  * get_list_length - get the length of the linked list
  * @head: header node
  *
@@ -44,6 +22,31 @@ int get_list_length(listint_t **head)
 }
 
 /**
+ * inverse_list - inverse a linked list
+ * @head: header node
+ *
+ * Return: reversed linked list
+ */
+listint_t *inverse_list(listint_t *head)
+{
+	listint_t *original_list, *rev_list, *copy_current_node, *next_rev_node;
+
+	original_list = head;
+	original_list = original_list->next;
+
+	rev_list = head;
+	rev_list->next = NULL;
+
+	while (original_list)
+	{
+		copy_current_node = original_list;
+		original_list = original_list->next;
+		copy_current_node->next = rev_list;
+		rev_list = copy_current_node;
+	}
+	return (rev_list);
+}
+/**
  * is_palindrome - check if the linked list is palindromique
  * @head: header node
  *
@@ -51,44 +54,36 @@ int get_list_length(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-	int mid_length, list_length, counter, odd_length, n_value;
-	listint_t *current;
+	listint_t *original_list, *rev_list;
+	int mid_length, length_list, count, odd_length;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
 		return (1);
-
-	if (*head == NULL)
-		return (0);
-	list_length = get_list_length(head);
-	if (list_length == 1)
-		return (1);
-	odd_length = list_length % 2 == 0 ? 0 : 1;
-	mid_length = list_length / 2;
-	counter = 1;
-	current = *head;
-
-	while (counter != mid_length)
+	length_list = get_list_length(head);
+	mid_length = length_list / 2;
+	odd_length = length_list % 2 == 0 ? 0 : 1;
+	original_list = *head;
+	count = 0;
+	while (count != mid_length - 1)
 	{
-		current = current->next;
-		counter++;
+		original_list = original_list->next;
+		count++;
 	}
-
-	if (odd_length)
-		counter--;
-	current = current->next;
-
-	while (current)
+	if (odd_length == 0)
 	{
-
-		n_value = get_node_number(head, counter);
-
-		if (n_value == current->n)
-		{
-			current = current->next;
-			counter--;
-		}
-		else
+		if (original_list->n != original_list->next->n)
 			return (0);
 	}
+
+	rev_list = inverse_list(*head);
+	original_list = *head;
+	while (original_list)
+	{
+		if (rev_list->n != original_list->n)
+			return (0);
+		rev_list = rev_list->next;
+		original_list = original_list->next;
+	}
+
 	return (1);
 }
