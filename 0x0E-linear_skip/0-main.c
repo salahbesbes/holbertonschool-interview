e6946610900bfbf83d5ae3bdc8c563a798d647ec
salahbesbes <salah_besbes@ymail.com>
2021-11-04 15:49:53 +0100

    skip linked list

--- 0x0E-linear_skip/0-main.c
+++ 0x0E-linear_skip/0-main.c
@@ -16,7 +16,9 @@ int main(void)
 {
 	skiplist_t *list, *res;
 	int array[] = {
-		0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 76, 99};
+		0,
+		1,
+	};
 	size_t size = sizeof(array) / sizeof(array[0]);
 
 	list = create_skiplist(array, size);
@@ -25,8 +27,8 @@ int main(void)
 	res = linear_skip(list, 53);
 	printf("Found %d at index: %p\n", 53, (void *)res);
 
-	res = linear_skip(list, 2);
-	printf("Found %d at index: %lu\n\n", 2, res->index);
+	res = linear_skip(list, 1);
+	printf("Found %d at index: %lu\n\n", 1, res->index);
 	res = linear_skip(list, 999);
 	printf("Found %d at index: %p\n", 999, (void *)res);
 
