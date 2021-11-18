cd8b7b7da9d60a56736d7f1f4fbc91aa73635d0c
salahbesbes <salah_besbes@ymail.com>
2021-11-18 19:39:59 +0100

    update

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -2,6 +2,18 @@
 """ Rain based on walls """
 
 
+def getIndexFarestAdjacentRightWall(wallIndex, walls):
+    """ walls = [0, 3, 2, 2, 0, 0, 2, 2, 3] this case is not handled yet  """
+    adjacent = wallIndex
+    while (adjacent + 1 != len(walls)):
+        if walls[adjacent + 1] != 0 and walls[adjacent] != 0:
+            adjacent += 1
+        else:
+            break
+
+    return adjacent - wallIndex
+
+
 def getVolumeBetweenTowWals(firstWall, walls):
     """ calculate the water between the first wall and the first next wall """
     firstWallHeight = walls[firstWall]
@@ -9,10 +21,11 @@ def getVolumeBetweenTowWals(firstWall, walls):
     heightContainer = 0
     for index, wall in enumerate(walls[firstWall + 1:]):
         if wall != 0:
+
             widthContainer = index
-            heightContainer = min(firstWallHeight, wall)
+            heightContainer = min(firstWallHeight, walls[index])
             return {"containerVolume": widthContainer * heightContainer,
-                    "nexWallIndex": index}
+                    "nextWallIndex": index}
 
 
 def rain(walls):
@@ -23,9 +36,11 @@ def rain(walls):
 
     waterRetained = 0
     for index in range(0, len(walls)):
+
         res = getVolumeBetweenTowWals(index, walls)
         if (res is None):
             break
         waterRetained += res["containerVolume"]
-        index = res["nexWallIndex"] - 1
+        index = res["nextWallIndex"] - 1
+
     return waterRetained
--- 0x10-rain/0_main.py
+++ 0x10-rain/0_main.py
@@ -7,7 +7,21 @@ rain = __import__('0-rain').rain
 if __name__ == "__main__":
     # walls = [0, 1, 0, 2, 0, 3, 0, 4]
     # print(rain(walls))
-    walls = [2, 0, 0, 0, 0, 0, 0, 2]
+    # walls = []
+    # print(rain(walls))
+    # walls = [0]
+    # print(rain(walls))
+    # walls = [0, 0]
+    # print(rain(walls))
+    # walls = [0, 0, 1]
+    # print(rain(walls))
+    # walls = [0, 2, 0]
+    # print(rain(walls))
+    # walls = [0, 2, 0, 0, 2]
+    # print(rain(walls))
+    # walls = [0, 2, 0, 0, 2, 0]
+    # print(rain(walls))
+    walls = [0, 3, 2, 2, 0, 0, 2, 2, 3]
     print(rain(walls))
 
     # walls = [1, 0, 2]
