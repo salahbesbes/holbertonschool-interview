38ee2c3b72867b9d7359cb9b6588b694b946b657
salahbesbes <salah_besbes@ymail.com>
2021-11-18 18:58:38 +0100

    init

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Rain based on walls """
+
+
+def getVolumeBetweenTowWals(firstWall, walls):
+    """ calculate the water between the first wall and the first next wall """
+    firstWallHeight = walls[firstWall]
+    widthContainer = 0
+    heightContainer = 0
+    for index, wall in enumerate(walls[firstWall + 1:]):
+        if wall != 0:
+            widthContainer = index
+            heightContainer = min(firstWallHeight, wall)
+            return {"containerVolume": widthContainer * heightContainer, "nexWallIndex": index}
+
+
+def rain(walls):
+    """ calculate the tetained water """
+    waterRetained = 0
+    for index in range(0, len(walls)):
+        res = getVolumeBetweenTowWals(index, walls)
+        if (res is None):
+            break
+        waterRetained += res["containerVolume"]
+        index = res["nexWallIndex"] - 1
+    return waterRetained
