b51a23b457af697476c13432f8b1039239f596ba
salahbesbes <salah_besbes@ymail.com>
2021-12-21 23:15:46 +0100

    string multiplication

---
+++ 0x15-infinite_multiplication/main.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * reverse - inverse an array
+ *
+ * @arr: array
+ * @n: length
+ */
+void reverse(char arr[], int n)
+{
+	int aux[n], i;
+
+	for (i = 0; i < n; i++)
+	{
+		aux[n - 1 - i] = arr[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		arr[i] = aux[i];
+	}
+}
+/**
+ * printResult- print a readable string from array
+ *
+ * @arr: Result Array
+ * @length: length of array
+ */
+void printResult(char *arr, int length)
+{
+	int i = 0;
+
+	if (arr[0] == 0)
+	{
+		i = 1;
+	}
+	for (; i < length; i++)
+	{
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+}
+/**
+ * string_length - length of string
+ *
+ * @pointer: string
+ * Return: int
+ */
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	for (c = 0; pointer[c] != '\0'; c++)
+	{
+		if (pointer[c] < '0' || pointer[c] > '9')
+		{
+			return (-1);
+		}
+	}
+	return (c);
+}
+
+/**
+ * multiply - multiply 2 strings
+ *
+ * @n1: string1
+ * @n1L:  length str1
+ * @n2:  string 2
+ * @n2L:  length str2
+ * @res:  Result Array array
+ */
+void multiply(char *n1, int n1L, char *n2, int n2L, char *res)
+{
+	int i, j, digit;
+
+	for (i = 0; i < n1L; i++)
+	{
+		for (j = 0; j < n2L; j++)
+		{
+			digit = (n1[i] - '0') * (n2[j] - '0');
+			res[i + j] += digit;
+			res[i + j + 1] += res[i + j] / 10;
+			res[i + j] = (res[i + j] % 10);
+		}
+	}
+}
+/**
+ * main - check the code
+ * @argc: nb of argument
+ * @argv: list of argumets(strings)
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	char *arg1 = argv[1], *arg2 = argv[2];
+	int n1L, n2L;
+	char *n1, *n2, *res;
+	int i, j, digit;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n1L = string_length(arg1);
+	n2L = string_length(arg2);
+	if (n1L == -1 || n2L == -1)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((n1L == 1 || n2L == 1) && (*arg1 == '0' || *arg2 == '0'))
+	{
+		printf("0\n");
+		return (0);
+	}
+	n1 = malloc(sizeof(int) * n1L);
+	n2 = malloc(sizeof(int) * n2L);
+	res = malloc(sizeof(int) * (n1L + n2L));
+	for (i = 0; i < n1L; i++)
+		n1[i] = arg1[i];
+
+	for (i = 0; i < n2L; i++)
+		n2[i] = arg2[i];
+	reverse(n1, n1L);
+	reverse(n2, n2L);
+	for (i = 0; i < (n1L + n2L); i++)
+		res[i] = 0;
+	multiply(n1, n1L, n2, n2L, res);
+	reverse(res, n1L + n2L);
+	printResult(res, n1L + n2L);
+	free(n1);
+	free(n2);
+	free(res);
+
+	return (0);
+}
