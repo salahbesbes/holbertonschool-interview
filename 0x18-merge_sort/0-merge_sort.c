a27bc4f05ee1085ceb24a24712aa89e6b0bb3459
salahbesbes <salah_besbes@ymail.com>
2022-01-19 16:06:06 +0100

    update

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -15,63 +15,44 @@ void merge(int *arr, int start, int mid, int end)
 {
 	int k, i, j, *newArray;
 
-	i = start;
-	k = 0;
-	j = mid + 1;
-	newArray = malloc(sizeof(int) * (end - start + 1));
-
-	while (i <= mid && j <= end)
+	i = start, k = 0, j = mid;
+	newArray = malloc(sizeof(int) * (end - start));
+	while (i < mid && j < end)
 	{
 		if (arr[j] <= arr[i])
 		{
 			newArray[k] = arr[j];
-			k++;
 			j++;
 		}
 		else
 		{
 			newArray[k] = arr[i];
-			k++;
 			i++;
 		}
+		k++;
 	}
-
-	while (j <= end)
+	while (j < end)
 	{
 		newArray[k] = arr[j];
 		k++;
 		j++;
 	}
-	while (i <= mid)
+	while (i < mid)
 	{
 		newArray[k] = arr[i];
 		k++;
 		i++;
 	}
 	printf("[Done]: ");
-	for (i = start; i <= end; i += 1)
+	for (i = start; i < end; i += 1)
 	{
 		arr[i] = newArray[i - start];
 		printf("%d", arr[i]);
-		if (i < end)
+		if (i < end - 1)
 			printf(", ");
 	}
 	printf("\n");
-
 	free(newArray);
-}
-
-void printArrayFrom(int *arr, int start, int end)
-{
-	int i = start;
-	while (arr && i <= end)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", arr[i]);
-		++i;
-	}
-	printf("\n");
 }
 
 /**
@@ -86,31 +67,30 @@ void recursive_merge_sort(int *arr, int start, int end)
 {
 	int mid, i;
 
-	if (end > start)
+	if (end - start <= 1)
+		return;
+	mid = (end + start) / 2;
+	recursive_merge_sort(arr, start, mid);
+	recursive_merge_sort(arr, mid, end);
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	for (i = start; i < mid; i++)
 	{
-		mid = (end + start) / 2;
-		recursive_merge_sort(arr, start, mid);
-		recursive_merge_sort(arr, mid + 1, end);
-
-		printf("Merging...\n");
-		printf("[left]: ");
-		for (i = start; i < mid + 1; i++)
-		{
-			printf("%d", arr[i]);
-			if (i < mid)
-				printf(", ");
-		}
-
-		printf("\n[right]: ");
-		for (i = mid + 1; i <= end; i++)
-		{
-			printf("%d", arr[i]);
-			if (i < end)
-				printf(", ");
-		}
-		printf("\n");
-		merge(arr, start, mid, end);
+		printf("%d", arr[i]);
+		if (i < mid - 1)
+			printf(", ");
 	}
+
+	printf("\n[right]: ");
+	for (i = mid; i < end; i++)
+	{
+		printf("%d", arr[i]);
+		if (i < end - 1)
+			printf(", ");
+	}
+	printf("\n");
+	merge(arr, start, mid, end);
 }
 
 /**
@@ -127,5 +107,5 @@ void merge_sort(int *array, size_t size)
 	int length;
 
 	length = (int)size;
-	recursive_merge_sort(array, 0, length - 1);
+	recursive_merge_sort(array, 0, length);
 }
