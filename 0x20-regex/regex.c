a1734390d1462aa6aeb1e7f761f04dbc49548ff1
salahbesbes <salah_besbes@ymail.com>
2022-03-23 13:22:17 +0100

    update

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,24 +1,43 @@
-#include "regex.h"
+#include <stdlib.h>
 
 /**
- * regex_match - regex search
- *
- * @str: string with no "*" or "."
- * @pattern: pattern
- *
- * Return: 1 if found else 0
+ * regex_match - checks whether a given pattern matches
+ * a given string
+ * @str: the given string
+ * @pattern: the given pattern
+ * Return: 1 if the pattern matches the string, or
+ * 0 if it doesn’t
  */
 int regex_match(char const *str, char const *pattern)
 {
 	if (str == NULL || pattern == NULL)
 		return (0);
-
 	if (*str == '\0' && *pattern == '\0')
 		return (1);
-
 	if (*str == *pattern)
 		return (regex_match(str + 1, pattern + 1));
-	else if (*(pattern + 1) == '*')
+
+	if (pattern[0] == '*')
+	{
+		if (pattern[1] == '*')
+			return (regex_match(str, pattern + 1));
+		if (regex_match(str + 1, pattern))
+			return (1);
+		else
+			return (regex_match(str, pattern + 1));
+	}
+
+	if (pattern[0] == '.')
+	{
+		if (pattern[1] == '*')
+			return (regex_match(str, pattern + 1));
+		if (regex_match(str + 1, pattern))
+			return (1);
+		else
+			return (regex_match(str, pattern + 1));
+	}
+
+	if (pattern[1] == '*')
 	{
 		if (pattern[0])
 			return (regex_match(str, pattern + 1));
@@ -27,26 +46,6 @@ int regex_match(char const *str, char const *pattern)
 		else
 			return (regex_match(str, pattern + 1));
 	}
-	if (*pattern == '*')
-	{
-		if (*(pattern + 1) == '*')
-			return (regex_match(str, pattern + 1));
 
-		if (regex_match(str + 1, pattern))
-			return (1);
-		else
-			return (regex_match(str, pattern + 1));
-	}
-
-	if (*pattern == '.')
-	{
-
-		if (pattern[1] == '*')
-			return (regex_match(str, pattern + 1));
-		if (regex_match(str + 1, pattern))
-			return (1);
-		else
-			return (regex_match(str, pattern + 1));
-	}
 	return (0);
 }
