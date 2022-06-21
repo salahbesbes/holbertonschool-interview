96765edcec7b056f2162555bc3b7adbe6ffc599f
salahbesbes <salah_besbes@ymail.com>
2022-06-21 12:38:50 +0200

    prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,5 +1,32 @@
-#!/usr/bin/python3
+""" Prime Game """
 
 
 def isWinner(x, nums):
-    pass
+    """ Returns name of the player that won the most rounds: Maria or Bill """
+
+    if x < 1 or not nums:
+        return None
+
+    n = max(nums)
+    player = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(n**0.5) + 1):
+        if not player[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            player[j] = False
+
+    player[0] = player[1] = False
+    c = 0
+    for i in range(len(player)):
+        if player[i]:
+            c += 1
+        player[i] = c
+
+    result = 0
+    for n in nums:
+        result += player[n] % 2 == 1
+    if result * 2 == len(nums):
+        return None
+    if result * 2 > len(nums):
+        return "Maria"
+    return "Ben"
